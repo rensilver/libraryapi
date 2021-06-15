b16f0bdafbda88a4b8344f5f2355dd6e5e662fc3
rensilver <ren.oliv87@gmail.com>
2021-06-15 17:08:28 -0300

    Project created

---
+++ src/test/java/com/rensilver/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rensilver.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

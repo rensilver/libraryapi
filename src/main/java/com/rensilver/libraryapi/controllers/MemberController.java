aa4e059ce426522f6398bb956df8e780831ffa5e
rensilver <ren.oliv87@gmail.com>
2021-06-15 22:45:12 -0300

    POST Inserting member

--- src/main/java/com/rensilver/libraryapi/controllers/MemberController.java
+++ src/main/java/com/rensilver/libraryapi/controllers/MemberController.java
@@ -1,5 +1,6 @@
 package com.rensilver.libraryapi.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rensilver.libraryapi.dto.MemberDTO;
 import com.rensilver.libraryapi.entities.Member;
@@ -35,5 +39,12 @@ public class MemberController {
 		return ResponseEntity.ok().body(new MemberDTO(member));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Void> findById(@RequestBody MemberDTO memberDto) {
+		Member member = memberService.fromDTO(memberDto);
+		member = memberService.insertMember(member);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(member.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 }
--- src/main/java/com/rensilver/libraryapi/services/MemberService.java
+++ src/main/java/com/rensilver/libraryapi/services/MemberService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rensilver.libraryapi.dto.MemberDTO;
 import com.rensilver.libraryapi.entities.Member;
 import com.rensilver.libraryapi.exceptions.MemberNotFoundException;
 import com.rensilver.libraryapi.repositories.MemberRepository;
@@ -23,5 +24,13 @@ public class MemberService {
 		Member member = memberRepository.findById(id)
 				.orElseThrow(() -> new MemberNotFoundException(id));
 		return member;
+	}
+	
+	public Member insertMember(Member entity) {
+		return memberRepository.insert(entity);
+	}
+	
+	public Member fromDTO(MemberDTO memberDto) {
+		return new Member(memberDto.getId(), memberDto.getName(), memberDto.getCpf());
 	}
 }
